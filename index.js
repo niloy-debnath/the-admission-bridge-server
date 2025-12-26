4db1162a2db5a4c438898bfc385c340ee7e7c22c
niloy-debnath <niloydebnathofficial@gmail.com>
2025-12-26 18:36:07 +0600

    universities apis separted and deleted the api route from index.js

--- index.js
+++ index.js
@@ -32,44 +32,44 @@ app.get("/test-db", async (req, res) => {
   }
 });
 
-// Universities route
-app.get("/universities", async (req, res) => {
-  try {
-    const { minFee, maxFee, gpa, ielts, country, degree } = req.query;
+// // Universities route
+// app.get("/universities", async (req, res) => {
+//   try {
+//     const { minFee, maxFee, gpa, ielts, country, degree } = req.query;
 
-    const minFeeNum = minFee ? Number(minFee) : 0;
-    const maxFeeNum = maxFee ? Number(maxFee) : 100000;
+//     const minFeeNum = minFee ? Number(minFee) : 0;
+//     const maxFeeNum = maxFee ? Number(maxFee) : 100000;
 
-    let query = `SELECT * FROM universities WHERE tuition_fee BETWEEN $1 AND $2`;
-    const values = [minFeeNum, maxFeeNum];
-    let idx = 3;
+//     let query = `SELECT * FROM universities WHERE tuition_fee BETWEEN $1 AND $2`;
+//     const values = [minFeeNum, maxFeeNum];
+//     let idx = 3;
 
-    if (country && country.trim() !== "") {
-      query += ` AND LOWER(country) LIKE LOWER($${idx})`;
-      values.push(`%${country}%`);
-      idx++;
-    }
+//     if (country && country.trim() !== "") {
+//       query += ` AND LOWER(country) LIKE LOWER($${idx})`;
+//       values.push(`%${country}%`);
+//       idx++;
+//     }
 
-    if (degree && degree.trim() !== "") {
-      query += ` AND LOWER(degree_level) LIKE LOWER($${idx})`;
-      values.push(`%${degree}%`);
-      idx++;
-    }
+//     if (degree && degree.trim() !== "") {
+//       query += ` AND LOWER(degree_level) LIKE LOWER($${idx})`;
+//       values.push(`%${degree}%`);
+//       idx++;
+//     }
 
-    const result = await pool.query(query, values);
+//     const result = await pool.query(query, values);
 
-    const universities = result.rows.map((u) => ({
-      ...u,
-      eligible:
-        (!gpa || Number(gpa) >= u.min_gpa) &&
-        (!ielts || Number(ielts) >= u.min_ielts),
-    }));
+//     const universities = result.rows.map((u) => ({
+//       ...u,
+//       eligible:
+//         (!gpa || Number(gpa) >= u.min_gpa) &&
+//         (!ielts || Number(ielts) >= u.min_ielts),
+//     }));
 
-    res.json(universities);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).json({ message: "Database error" });
-  }
-});
+//     res.json(universities);
+//   } catch (err) {
+//     console.error(err.message);
+//     res.status(500).json({ message: "Database error" });
+//   }
+// });
 
-export default serverless(app);
+// export default serverless(app);
